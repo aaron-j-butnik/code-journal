3ae2ab50abdf6ad43d9b45055a7eab7a9904c5d3
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-01 07:54:16 +0000

    Added event listener for submit events on entry form, put form's inputs into new object, added nextEntryID to object, incremented nextEntryID on data model.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 var $imageSubmit = document.querySelector('.image-submit');
 var $imageUpdate = document.querySelector('img');
-// var $submitForm = document.querySelector('.user-entry-form');
+var $submitForm = document.querySelector('#user-entry-form');
 
 $imageSubmit.addEventListener('input', handlePhotoUpdate);
-// $submitForm = addEventListener('submit', handleSubmit);
+$submitForm.addEventListener('submit', handleSubmit);
 
 function handlePhotoUpdate(event) {
   $imageUpdate.setAttribute('src', event.target.value);
 }
 
-// function handleSubmit(event) {
+function handleSubmit(event) {
+  event.preventDefault();
+  var userEntryData = {};
 
-// }
+  userEntryData.title = $submitForm.elements.title.value;
+  userEntryData.url = $submitForm.elements.url.value;
+  userEntryData.notes = $submitForm.elements.notes.value;
+  userEntryData.entryId = data.nextEntryId;
+  data.nextEntryId++;
+
+  // console.log(userEntryData);
+  $submitForm.reset();
+}
