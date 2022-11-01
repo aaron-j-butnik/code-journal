ee39db67b0ec2d6e46fe3921e610638c5e6715c1
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-01 04:53:05 +0000

    Cleaned up HTML and CSS for entry form.  Added event listener on  input for image url, and set img element src to update with user inputed url. #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
 var $imageSubmit = document.querySelector('.image-submit');
 var $imageUpdate = document.querySelector('img');
+// var $submitForm = document.querySelector('.user-entry-form');
 
 $imageSubmit.addEventListener('input', handlePhotoUpdate);
+// $submitForm = addEventListener('submit', handleSubmit);
 
 function handlePhotoUpdate(event) {
   $imageUpdate.setAttribute('src', event.target.value);
 }
+
+// function handleSubmit(event) {
+
+// }
