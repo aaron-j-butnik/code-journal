3718a2c512b506870fb9b3cec0934c8340353583
Aaron Butnik <aaronjbutnik@gmail.com>
2022-11-02 17:28:23 +0000

    Cleaned up some HTML and CSS. Defined function renderEntries, which returns a DOM tree that matches an example entry in the HTML. #2

--- js/main.js
+++ js/main.js
@@ -23,3 +23,44 @@ function handleSubmit(event) {
   $imageUpdate.setAttribute('src', 'images/placeholder-image-square.jpg');
   $submitForm.reset();
 }
+
+// DOM tree creation
+
+function renderEntries(entries) {
+  var liColumnFull = document.createElement('li');
+  liColumnFull.setAttribute('class', 'column-full');
+
+  var divRow = document.createElement('div');
+  divRow.setAttribute('class', 'row');
+
+  var divImg = document.createElement('div');
+  divImg.setAttribute('class', 'column-half');
+
+  var imgEntryEl = document.createElement('img');
+  imgEntryEl.setAttribute('src', data.entries[1].url);
+
+  var h3Title = document.createElement('h3');
+  var h3Content = document.createTextNode(data.entries[1].title);
+  h3Title.setAttribute('class', 'entry-list-title');
+
+  var divText = document.createElement('div');
+  divText.setAttribute('class', 'column-half');
+
+  var paragraph = document.createElement('p');
+  var pContent = document.createTextNode(data.entries[1].notes);
+  paragraph.setAttribute('class', 'entry-list-notes');
+
+  liColumnFull.appendChild(divRow);
+  divRow.appendChild(divImg);
+  divRow.appendChild(divText);
+  divImg.appendChild(imgEntryEl);
+  divText.appendChild(h3Title);
+  divText.appendChild(paragraph);
+
+  h3Title.appendChild(h3Content);
+  paragraph.appendChild(pContent);
+
+  return liColumnFull;
+}
+
+renderEntries();
